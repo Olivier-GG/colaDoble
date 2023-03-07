08b54bf9d0fa10a90f39d3efee3260447892e41f
Olivier <oliviergg@uma.es>
2023-03-07 20:35:42 +0100

    Añado algunos nested

--- src/test/java/org/mps/deque/DequeNodeTest.java
+++ src/test/java/org/mps/deque/DequeNodeTest.java
@@ -10,6 +10,7 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.condition.DisabledIfSystemProperties;
 import org.junit.jupiter.api.condition.EnabledForJreRange;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -31,105 +32,134 @@ public class DequeNodeTest {
         node1 = new DequeNode(5,null,node2);
         node2 = new DequeNode(7,node1,node3);
         node3 = new DequeNode(1,node2,null);
+
     }
 
+    @AfterEach
+    void shutdown () {
+        node1 = null;
+        node2 = null;
+        node3 = null;
+    }
 
-    @Test
-    public void testSetGetItem () {
+    @Nested
+    @DisplayName("Getter y setter")
+    class getterYSetter {
 
-        node1.setItem(33);
+        @Test
+        @DisplayName("Test get item")
+        public void testGetItem () {
+            assertEquals(node1.getItem(),5);
+            assertEquals(node2.getItem(),7);
+            assertEquals(node3.getItem(),1);
+        }
 
-        Object obtainedValue = node1.getItem();
-        assertEquals(33,(int)obtainedValue);
+        @Test
+        @DisplayName("Test set item")
+        public void testSetItem () {
 
+            node1.setItem(33);
+
+            Object obtainedValue = node1.getItem();
+            assertEquals(33,(int)obtainedValue);
+
+        }
     }
 
 
     @Nested
-    @DisplayName("PreviousTests")
-    class previous {
+    @DisplayName("Test Next y previous")
+    class NextPrevious {
+
+        @Nested
+        @DisplayName("Previous Tests")
+        class previous {
+
+            @Test
+            @DisplayName("test Get Previous")
+            void testGetPrevious () {
+
+                DequeNode prueba1 = node1.getPrevious();
+                DequeNode prueba2 = node2.getPrevious();
+                DequeNode prueba3 = node3.getPrevious();
+
+                assertEquals(prueba1,null);
+                assertEquals(prueba2,node1);
+                assertEquals(prueba3,node2);
+
+            }
+
+            @Test
+            @DisplayName("test Set Previous")
+            void testSetPrevious () {
+
+                node1.setPrevious(node3);
+
+                assertEquals(node1.getPrevious(),node3);
+
+            }
+
+
+        }
+
+        //Otro Nested, con el de arriba en un pack mas grande
+        @Nested
+        @DisplayName("Next tests")
+        class next {
+            @DisplayName("test Get Next")
+            @Test
+            void testGetNext () {
+
+                node1.setNext(node2);
+
+                assertEquals(node1.getNext(),node2);
+
+            }
+            @Test
+            @DisplayName("test Set Next")
+            void testSetNext () {
+
+                node3.setNext(node2);
+
+                assertEquals(node3.getNext(),node2);
+            }
+        }
+
+    }
+
+
+
+
+    @Nested
+    @DisplayName("Tests posiciones booleanos")
+    class booleanos {
+        @Test
+        void testIsFirstNode () {
+
+            assertTrue(node1.isFirstNode());
+            assertFalse(node2.isFirstNode());
+            assertFalse(node3.isFirstNode());
+        }
 
         @Test
-        @DisplayName("Get")
-        void testGetPrevious () {
+        void testIsLastNode () {
 
-            DequeNode prueba1 = node1.getPrevious();
-            DequeNode prueba2 = node2.getPrevious();
-            DequeNode prueba3 = node3.getPrevious();
-
-            assertEquals(prueba1,null);
-            assertEquals(prueba2,node1);
-            assertEquals(prueba3,node2);
+            assertFalse(node1.isLastNode());
+            assertFalse(node2.isLastNode());
+            assertTrue(node3.isLastNode());
 
         }
 
         @Test
-        @DisplayName("Set")
-        void testSetPrevious () {
+        void isNotTerminalNode () {
 
-            node1.setPrevious(node3);
+            assertFalse(node1.isNotATerminalNode());
+            assertTrue(node2.isNotATerminalNode());
+            assertFalse(node3.isNotATerminalNode());
 
-            assertEquals(node1.getPrevious(),node3);
-
-        }
-
-        @AfterEach
-        void shutdown () {
-            node1 = null;
-            node2 = null;
-            node3 = null;
         }
     }
 
-
-
-    //Otro Nested, con el de arriba en un pack mas grande
-
-    @Test
-    void testGetNext () {
-
-        assertEquals(node1.getNext(),node2);
-
-    }
-    @Test
-    void testSetNext () {
-
-        node3.setNext(node2);
-
-        assertEquals(node3.getNext(),node2);
-    }
-
-
-
-    //Nested de los siguientes 3
-
-    @Test
-    void testIsFirstNode () {
-
-        assertTrue(node1.isFirstNode());
-        assertFalse(node2.isFirstNode());
-        assertFalse(node3.isFirstNode());
-    }
-
-    @Test
-    void testIsLastNode () {
-
-        assertFalse(node1.isLastNode());
-        assertFalse(node2.isLastNode());
-        assertTrue(node3.isLastNode());
-
-    }
-
-    @Test
-    void isNotTerminalNode () {
-
-        node1.setNext(null);
-
-        assertFalse(node1.isNotATerminalNode());
-        assertTrue(node2.isNotATerminalNode());
-        assertTrue(node3.isNotATerminalNode());
-
-    }
 
 
 }
